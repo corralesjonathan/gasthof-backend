d79e2690553f324378f055aa67e82c84a51b0a43
corralesjonathan <jonathan.corralesruiz@ucr.ac.cr>
2023-11-11 16:26:11 -0600

    update imgs folder

--- home.php
+++ home.php
@@ -16,65 +16,9 @@
 
 <body>
     <!--header & hero-->
-    <header class="hero-background">
-        <!--nav-->
-        <div class="top-nav-container">
-            <nav class="top-nav">
-                <button id="mobile-open-btn"> <img src="./imgs/icons/mobile-btn.svg" alt="Mobile button"></button>
-                <div class="website-logo-container">
-                    <img src="./imgs/icons/website-logo.svg" alt="Gasthof logo" class="website-logo">
-                </div>
-                <!--nav menu & mobile btn-->
-                <div id="nav-container">
-                    <button id="mobile-close-btn"><img src="./imgs/icons/close.svg" alt=""></button>
-                    <ul class="nav-list">
-                        <li><a id="btnHome" class="nav-list-link" href="home.html">Home</a></li>
-                        <li><a id="btnMenu" class="nav-list-link" href="menu.html">Menu</a></li>
-                        <li><a id="btnContact" class="nav-list-link" href="#">Contact</a></li>
-                        <li><a id="btnAboutUs" class="nav-list-link" href="#">About Us</a></li>
-                    </ul>
-                </div>
-                <!--nav menu & mobile btn-->
-
-                <!--nav icons-->
-                <div class="icons_container">
-                    <a class="search-icon" href="#"><img src="./imgs/icons/search-icon.svg" alt="Search"></a>
-                    <a class="user-icon" href="#"><img src="./imgs/icons/user.svg" alt="Account"></a>
-                    <a class="bag-icon" href="#"><img src="./imgs/icons/bag.svg" alt="Bag"></a>
-                </div>
-                <!--nav icons-->
-            </nav>
-        </div>
-        <!--nav-->
-        <!--hero slider, titles & buttons-->
-        <div class="hero-container">
-            <div class="hero-title-container">
-                <div class="hero-img-container">
-                    <div class="hero-slide fade">
-                        <img class="hero-img" src="./imgs/hero-dish.png" alt="Hero dish">
-                    </div>
-                    <div class="hero-slide fade">
-                        <img class="hero-img" src="./imgs/hero-dish01.png" alt="Hero dish">
-                    </div>
-                    <div class="hero-dots-container">
-                        <span class="hero-dot" onclick="currentHeroSlide(1)"></span>
-                        <span class="hero-dot" onclick="currentHeroSlide(2)"></span>
-                    </div>
-                </div>
-                <div class="hero-texts-container">
-                    <h3 class="hero-title1">your passport to</h3>
-                    <h1 class="hero-title2">german dining</h1>
-                    <p class="hero-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br> Pellentesque
-                        fermentum tempor accumsan.</p>
-                    <div class="hero-buttons-container">
-                        <a href="#" class="btn">read more</a>
-                        <a href="menu.html" class="btn view-menu">view menu</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--hero slider, titles & buttons-->
-    </header>
+    <?php 
+        include './parts/header.php'
+    ?>
     <!--header & hero-->
 
     <!--main content-->
