eeae8e2b1339334d2af30e27f5e1c0f99ecc2202
corralesjonathan <jonathan.corralesruiz@ucr.ac.cr>
2023-11-16 00:53:02 -0600

    update pages titles

--- dish.php
+++ dish.php
@@ -29,7 +29,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dish</title>
+    <title><?php echo $dish[0]["dish_name"]?> - Gashof</title>
     <!--fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
