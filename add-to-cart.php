068778176a5b6a42c6e42e29e6ac84a9b4c8b716
corralesjonathan <jonathan.corralesruiz@ucr.ac.cr>
2023-12-01 01:00:41 -0600

    update cladd add-to-cart, now if dish is already in the cart, update quantity and subtotal, else add new dish

--- add-to-cart.php
+++ add-to-cart.php
@@ -8,20 +8,30 @@ if (isset($_SERVER["CONTENT_TYPE"])) {
         $content = trim(file_get_contents("php://input"));
         $decoded = json_decode($content, true);
 
-        $isInCart=$database->select("tb_cart", "*", [
+        $isInCart = $database->select("tb_cart", "*", [
             "AND" => [
                 "id_user" => $decoded["id_user"],
                 "id_dish" => $decoded["id_dish"],
             ],
         ]);
 
-        if($isInCart){
-            $updateCart = $database->update("tb_cart",[
-                "quantity" => 4,
-                "subtotal" => $decoded["subtotal"]*4
+        //if to verify if dish is already in the cart
+        if ($isInCart) {
+            // Dish is already in the cart, update quantity and subtotal
+            $currentSubtotal = $isInCart[0]['subtotal'];
+            $currentQuantity = $isInCart[0]['quantity'];
+            $newQuantity = $currentQuantity + $decoded["quantity"];
+
+            $database->update("tb_cart", [
+                "quantity" => $newQuantity,
+                "subtotal" => $currentSubtotal + $decoded["subtotal"],//is $decoded["subtotal"] because in jv code subtotal=dish_price*quantity
+            ], [
+                "id_user" => $decoded["id_user"],
+                "id_dish" => $decoded["id_dish"],
             ]);
-        }else{
-            $insertCart = $database->insert("tb_cart",[
+        } else {
+            // Dish is not in the cart, insert a new record
+            $insertCart = $database->insert("tb_cart", [
                 "id_user" => $decoded["id_user"],
                 "id_dish" => $decoded["id_dish"],
                 "quantity" => $decoded["quantity"],
@@ -29,6 +39,7 @@ if (isset($_SERVER["CONTENT_TYPE"])) {
             ]);
         }
 
+        // Retrieve and return the updated cart
         $cart = $database->select("tb_cart", [
             "[>]tb_dishes" => ["id_dish" => "id_dish"],
             "[>]tb_users" => ["id_user" => "id_user"],
